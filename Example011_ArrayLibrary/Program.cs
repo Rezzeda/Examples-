b3e7656223edb028c09d58062d30a7d1ff573486
Reseda <Reseda@examle.com>
2022-09-19 20:41:00 +0400

    Add Example011

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,51 @@
+﻿// Создаем массив и заполнем его случайными числами от 1 до 10, в нем ищем первый индекс элемента со значением 4
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+    {
+        int count = collection.Length;
+        int index =0;
+        int position = -1;
+        while (index < count)
+        {
+            if (collection[index] == find)
+            {
+                position = index;
+                break;
+            }
+            index++;
+        }
+        return position;
+    }
+
+int[] array = new int[10];
+
+FillArray(array);
+array[4] = 4;
+array[6] = 4;
+
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
